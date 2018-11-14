5439286f376b33a6ebdebb8765f1d30925ce204e
Rudy <peace1spirit@gmail.com>
2018-11-14 16:41:17 +0700

    add package

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 'use strict'
 
-
+require('dotenv').config();
 const express = require('express');
 const bodyParser =  require('body-parser');
 const cors = require('cors');
-require('dotenv').config();
 
 const app = express();
 app.use(cors());

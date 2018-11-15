4141335ff9e9bc714e91a9886e8f2b2b0978156f
Rudy <peace1spirit@gmail.com>
2018-11-15 10:33:20 +0700

    addpackage

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser =  require('body-parser');
 const cors = require('cors');
-
+// const getObject = require('./routes/detectobject.js');
+// const getTranslate = require('./routes/translate.js');
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
@@ -12,7 +13,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 
 
-
+// app.use('/detectobject', getObject);
+// app.use('/translate', getTranslate);
 
 app.listen(3000, () => {
     console.log(`Listening to port 3000`)

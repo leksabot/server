984608e2706b3fb52baf6e707f7ce433094d35b0
Rudy <peace1spirit@gmail.com>
2018-11-16 12:08:59 +0700

    add autodetect with gcp and without gcp

--- routes/detectobject.js
+++ routes/detectobject.js
@@ -1,10 +1,14 @@
 'use strict'
 
 const router = require('express').Router();
-const {DetectInfo,DetectText} = require('../controllers/detectObject');
+const {DetectInfo,DetectText, AutoGCP , AutoNoGCP} = require('../controllers/detectObject');
 const helpers = require('../helpers/images')
+const filemulter = require('../helpers/filemulter')
 
-router.post('/',  helpers.multer.single('imagefile'), helpers.sendUploadToGCS, DetectInfo);
+router.post('/',  filemulter.multer.single('imagefile'), AutoNoGCP);
+router.post('/gcp',  filemulter.multer.single('imagefile'), helpers.sendUploadToGCS, AutoGCP);
+router.post('/object',  helpers.multer.single('imagefile'), helpers.sendUploadToGCS, DetectInfo);
 router.post('/text',  helpers.multer.single('imagefile'), helpers.sendUploadToGCS, DetectText);
+
 
 module.exports = router;

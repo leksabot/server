0b8e17c23645cc3e909d93c855a1da07dcb3a3bc
esadeli <efrat.sadeli@gmail.com>
2018-11-17 17:25:58 +0700

    French Validator test OK

--- test/frenchValidator.test.js
+++ test/frenchValidator.test.js
@@ -1,16 +1,26 @@
-const chai = require('chai')
-const frenchValidator = require('../helpers/frenchValidator')
 const assert = require('assert')
+const frenchValidator = require('../helpers/frenchValidator')
 
 describe('French Validator', () => {
-
+    
     it(`should give status true if the question is started with 
         qu'-est-ce que c'est`, (done)=> {
-            var obj = frenchValidator(`
-            qu'est-ce que c'est`, `qu'est-ce que c'est un maison`)
+            var obj = frenchValidator('qu\'est-ce que c\'est un ', 
+                'qu\'est-ce que c\'est un maison')
 
-            assert.equal(obj.status, false)
-                
+            assert.equal(obj.status, true)
+            assert.equal(obj.word, 'maison')
+            // console.log('result------',obj)
+            done()    
         })
 
+    it(`should give status false if the question is started with 
+        qu'-est-ce que c'est`, (done)=> {
+            var obj = frenchValidator('qu\'est-ce que c\'est un', 
+                `what is dog`)
+
+            assert.equal(obj.status, false)
+            assert.equal(obj.word, '')
+            done()    
+        })    
 })
